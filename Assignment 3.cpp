3a773645ed0b1fd079ac6a15b16da092f6c59c0c
SAURABH MALI <sdmaniitian@gmail.com>
2025-03-14 12:12:30 +0530

    Update Assignment 3.cpp

--- Assignment 3.cpp
+++ Assignment 3.cpp
@@ -1,45 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
-// Function to perform BFS traversal in the maze graph
-void BFS(vector<vector<int>> &G, int start, int n) {
-    vector<int> visited(n, 0);
-    queue<int> Q;
 
-    cout << "BFS Traversal from " << start << ": ";
-    cout << start << " ";
-    visited[start] = 1;
-    Q.push(start);
+// Class representing a Maze Graph and its traversal algorithms
+class MazeGraph {
+private:
+    vector<vector<int>> G;  // Adjacency matrix representation of the maze
+    int n;                  // Number of nodes
 
-    while (!Q.empty()) {
-        int i = Q.front();
-        Q.pop();
-
-        for (int j = 1; j < n; j++) {
-            if (G[i][j] == 1 && visited[j] == 0) {
-                cout << j << " ";
-                visited[j] = 1;
-                Q.push(j);
-            }
-        }
+public:
+    // Constructor to initialize the maze graph
+    MazeGraph(vector<vector<int>> graph) {
+        G = graph;
+        n = graph.size();
     }
-    cout << endl;
-}
 
-// Function to perform DFS traversal in the maze graph
-void DFS(vector<vector<int>> &G, int start, int n, vector<int> &visited) {
-    if (visited[start] == 0) {
+    // Function to perform BFS traversal in the maze graph
+    void BFS(int start) {
+        vector<int> visited(n, 0);
+        queue<int> Q;
+
+        cout << "BFS Traversal from " << start << ": ";
         cout << start << " ";
         visited[start] = 1;
+        Q.push(start);
 
-        for (int j = 1; j < n; j++) {
-            if (G[start][j] == 1 && visited[j] == 0) {
-                DFS(G, j, n, visited);
+        while (!Q.empty()) {
+            int i = Q.front();
+            Q.pop();
+
+            for (int j = 1; j < n; j++) {
+                if (G[i][j] == 1 && visited[j] == 0) {
+                    cout << j << " ";
+                    visited[j] = 1;
+                    Q.push(j);
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    // Function to perform DFS traversal in the maze graph
+    void DFS(int start, vector<int> &visited) {
+        if (visited[start] == 0) {
+            cout << start << " ";
+            visited[start] = 1;
+
+            for (int j = 1; j < n; j++) {
+                if (G[start][j] == 1 && visited[j] == 0) {
+                    DFS(j, visited);
+                }
             }
         }
     }
-}
+
+    // Helper function to start DFS with fresh visited array
+    void startDFS(int start) {
+        vector<int> visited(n, 0);
+        cout << "DFS Traversal from " << start << ": ";
+        DFS(start, visited);
+        cout << endl;
+    }
+};
 
 int main() {
     // Graph representation of a sample maze (0 = no path, 1 = path exists)
@@ -53,12 +77,12 @@ int main() {
         {0, 0, 0, 0, 1, 0, 0}
     };
 
-    cout << "DFS Traversal from 4: ";
-    vector<int> visited(7, 0);
-    DFS(G, 4, 7, visited);
-    cout << endl;
+    // Creating an object of MazeGraph
+    MazeGraph maze(G);
 
-    BFS(G, 4, 7);
+    // Performing DFS and BFS traversals
+    maze.startDFS(4);
+    maze.BFS(4);
 
     return 0;
 }
